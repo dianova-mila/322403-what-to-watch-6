fa1a4342e8d84d70dda38b887064bcb9e2928372
Dianova Mila <dianova.mila.spb@gmail.com>
2021-02-06 22:54:16 +0300

    Созданы компоненты основных страниц приложения, и настроена маршрутизация.

---
+++ src/components/not-found/not-found.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <React.Fragment>
+      <div className="user-page">
+        <header className="page-header user-page__head">
+          <div className="logo">
+            <a href="main.html" className="logo__link">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+
+          <h1 className="page-title user-page__title">404. Page not found</h1>
+
+        </header>
+
+        <div className="user-page__content">
+          <Link className="catalog__button" to="/">Вернуться на главную</Link>
+        </div>
+
+        <footer className="page-footer">
+          <div className="logo">
+            <a href="main.html" className="logo__link logo__link--light">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
+          </div>
+        </footer>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default NotFound;
