21e0eaa5d919eb5134a8210429283966258da577
Dianova Mila <dianova.mila.spb@gmail.com>
2021-03-21 15:08:39 +0300

    Реализована авторизация, созданы приватные маршруты

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -6,4 +6,16 @@ const fetchMovies = () => (dispatch, _getState, api) => (
     .then(({data}) => dispatch(ActionCreator.loadMovies(adaptMoviesToClient(data))))
 );
 
-export {fetchMovies};
+const checkAuth = () => (dispatch, _getState, api) => (
+  api.get(`/login`)
+    .then(() => dispatch(ActionCreator.requireAuthorization(true)))
+    .catch(() => {})
+);
+
+const login = ({login: email, password}) => (dispatch, _getState, api) => (
+  api.post(`/login`, {email, password})
+    .then(() => dispatch(ActionCreator.requireAuthorization(true)))
+    .then(() => dispatch(ActionCreator.redirectToRoute(`/`)))
+);
+
+export {fetchMovies, checkAuth, login};
